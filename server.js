45c19eec60952a1aec94593f2da2e0700c916a6a
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-04-17 20:24:44 +0200

    Adds POST endpoint

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+
+app.use(express.json());
 
 let activities = [
   {
@@ -25,31 +27,58 @@ let activities = [
   },
 ];
 
-app.get('/', (request, response) => {
-  response.send('<h1>Yo worlds!</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Yo worlds!</h1>");
+});
 
-app.get('/activities', (request, response) => {
-  response.json(activities)
-})
+app.get("/activities", (request, response) => {
+  response.json(activities);
+});
 
-app.get('/activities/:id', (request, response) => {
-  const id = parseInt(request.params.id)
-  const activity = activities.find(activity => activity.id === id)
+app.get("/activities/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  const activity = activities.find((activity) => activity.id === id);
 
   if (activity) {
-    response.json(activity)
+    response.json(activity);
   } else {
-    response.status(400).end()
+    response.status(400).end();
   }
-})
+});
 
-app.delete('/activities/:id', (request, response) => {
-  const id = parseInt(request.params.id)
-  activities = activities.filter(activity => activity.id !== id)
+app.delete("/activities/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  activities = activities.filter((activity) => activity.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId =
+    activities.length > 0 ? Math.max(...activities.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/activities", (request, response) => {
+  const body = request.body
+  
+  if (!body.type || !body.length) {
+    return response.status(400).json({
+      error: "Missing param(s)"
+    })
+  }
+
+  const activity = {
+    type: body.type,
+    intervals: body.intervals || 1,
+    length: body.length,
+    date: new Date(),
+    id: generateId()
+  }
+
+  activities = activities.concat(activity);
+  response.json(activity);
+});
 
 const PORT = 3001;
 app.listen(PORT);
